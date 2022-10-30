7145a803b9b02611c0c27dbe8efce82d2fc5f06d
qwer083 <Qwertyiop890>
2022-10-30 18:49:40 +0300

    Initial commit

---
+++ src/test/java/Mortgage.java
@@ -0,0 +1,9 @@
+public class Mortgage {
+    public static void main(String []args) {
+        long a = 35000000L;
+        double b = 0.15;
+        long c = (long) (a*b);
+        String s = "сумма первоначального взноса";
+        System.out.println("сумма первоначального взноса: " + c);
+    }
+}
