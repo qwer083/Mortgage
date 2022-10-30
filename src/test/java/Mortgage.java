cf6437dab859653de774df8f9a889f32b9e45f42
qwer083 <Qwertyiop890>
2022-10-30 19:52:46 +0300

    final

--- src/test/java/Mortgage.java
+++ src/test/java/Mortgage.java
@@ -1,9 +1,13 @@
 public class Mortgage {
     public static void main(String []args) {
-        long a = 35000000L;
-        double b = 0.15;
+        final long a = 35000000L;
+        final double b = 0.15;
         long c = (long) (a*b);
-        String s = "сумма первоначального взноса";
+        String s = "стоимость квартиры";
+        String h = "процент первоначального взноса по ипотеке";
+        String j = "сумма первоначального взноса";
+        System.out.println("стоимость квартиры: " + a);
+        System.out.println("процент первоначального взноса по ипотеке: " + b);
         System.out.println("сумма первоначального взноса: " + c);
     }
 }
